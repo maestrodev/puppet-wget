9fd268646619cc347fcb1c54fa2712117fe3a3ac
Wayne Porter <wporter82@gmail.com>
2016-12-06 13:41:35 -0800

    Fix Ruby string error
    Ruby String class no longer has each. Update to convert the headers to
    an array in order to use each on them.
    
    Signed-off-by: Wayne Porter <wporter82@gmail.com>

--- manifests/fetch.pp
+++ manifests/fetch.pp
@@ -144,7 +144,15 @@ define wget::fetch (
 
   # again, not using stdlib.concat, concatanate array of headers into a single string
   if $headers != undef {
-    $headers_all = inline_template('<% @headers.each do | header | -%> --header "<%= header -%>"<% end -%>')
+    $array_headers = is_array($headers) ? {
+      false => $headers ? {
+        '' => [],
+        default => split($headers, ','),
+      },
+      default => $headers,
+    }
+
+    $headers_all = inline_template('<% @array_headers.each do | header | -%> --header "<%= header -%>"<% end -%>')
   }
 
   $header_option = $headers ? {
